62c0162b4d3c380c8d8f9af7ed5ca23b39ee2e91
Alireza Karimi <alirezayoom@gmail.com>
2023-12-17 21:31:29 +0430

    add aria label

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,11 @@ const Navbar = () => {
     <nav className={classes.navbar}>
       <div className={`${classes.container} container`}>
         <div>
-          <Link href="/" style={{ display: "flex", alignItems: "center" }}>
+          <Link
+            href="/"
+            aria-label="home"
+            style={{ display: "flex", alignItems: "center" }}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="32"
